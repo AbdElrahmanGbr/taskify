3b822c5e6daf123a629263bffcc8e150a6475053
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-12-23 09:02:31 +0200

    Added react-beautiful-dnd, and some styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,53 @@ import './App.css';
 import InputField from "./components/InputField";
 import {Todo} from "./model";
 import TodoList from "./components/TodoList";
+import {DragDropContext, DropResult} from "react-beautiful-dnd";
 
 function App() {
     const [todo, setTodo]: [string, ((value: (((prevState: string) => string) | string)) => void)] = useState("");
     const [todos, setTodos]: [Todo[], ((value: (((prevState: Todo[]) => Todo[]) | Todo[])) => void)] = useState<Todo[]>([])
+    const [completedTodos, setCompletedTodos]: [Todo[], ((value: (((prevState: Todo[]) => Todo[]) | Todo[])) => void)] = useState<Todo[]>([]);
     const handleAdd = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        setTodos([...todos, {id:Date.now(), todo:todo,isDone:false}]);
+        setTodos([...todos, {id: Date.now(), todo: todo, isDone: false}]);
         setTodo("");
     };
-    console.log('todos',todos);
-  return (
-    <div className="App">
-        <span className={"heading"}>Taskify</span>
-        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-        <TodoList todos={todos} setTodos={setTodos}/>
-    </div>
-  );
+    const onDragEnd = (result: DropResult) => {
+        const {destination, source} = result;
+        if (!destination) {
+            return;
+        }
+        if (destination.droppableId === source.droppableId && destination.index === source.index) {
+            return;
+        }
+        let add;
+        let active = todos;
+        let complete = completedTodos;
+        // Source Logic
+        if (source.droppableId === "TodosList") {
+            add = active[source.index];
+            active.splice(source.index, 1);
+        } else {
+            add = complete[source.index];
+            complete.splice(source.index, 1);
+        }
+        // Destination Logic
+        if (destination.droppableId === "TodosList") {
+            active.splice(destination.index, 0, add);
+        } else {
+            complete.splice(destination.index, 0, add);
+        }
+        setCompletedTodos(complete);
+        setTodos(active);
+    };
+    return (<DragDropContext onDragEnd={onDragEnd}>
+            <div className="App">
+                <span className={"heading"}>Taskify</span>
+                <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+                <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos}
+                          setCompletedTodos={setCompletedTodos}/>
+            </div>
+        </DragDropContext>);
 }
 
 export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,29 +2,46 @@ import React from 'react';
 import './style.css';
 import {Todo} from "../model";
 import SingleTodo from "./SingleTodo";
-interface Props{
+import {Droppable} from "react-beautiful-dnd";
+
+interface Props {
     todos: Todo[],
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
+    completedTodos: Todo[],
+    setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
-const TodoList = ({todos,setTodos}:Props) => {
+
+const TodoList = ({todos, setTodos, completedTodos, setCompletedTodos}: Props) => {
     return (
         <div className={"container"}>
-    <div className={"todos"}>
-<span className="todos__heading">Active Tasks</span>
-        {
-            todos.map((todo)=>(
-                <SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id}/>
-            ))}
-    </div>
-            <div className={"todos remove"}>
-                <span className="todos__heading">Completed Tasks</span>
+            <Droppable droppableId={"TodosList"}>
                 {
-                    todos.map((todo)=>(
-                        <SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id}/>
-                    ))}
-            </div>
+                    (provided, snapshot)=>(
+                        <div className={`todos ${snapshot.isDraggingOver? 'dragactive' : ''}`} ref={provided.innerRef} {...provided.droppableProps}>
+                            <span className="todos__heading">Active Tasks</span>
+                            {todos.map((todo, index) => (<SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id} index={index}/>))}
+                            {provided.placeholder}
+                        </div>
+                    )
+                }
+            </Droppable>
+            <Droppable droppableId={"TodosRemove"}>
+                {
+                    (provided, snapshot)=>(
+                        <div className={`todos remove ${snapshot.isDraggingOver? 'dragcomplete' : ''}`} ref={provided.innerRef} {...provided.droppableProps}>
+                            <span className="todos__heading">Completed Tasks</span>
+                            {completedTodos.map((todo, index) => (
+                                <s key={todo.id}>
+                                    <SingleTodo todo={todo} todos={completedTodos} setTodos={setCompletedTodos} index={index}/>
+                        </s>))}
+                            {provided.placeholder}
+                        </div>
+                    )
+                }
+            </Droppable>
 
         </div>
+
     );
 };
 
